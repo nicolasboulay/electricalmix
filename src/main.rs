8a265c5b0134dd20454dc84de7cc97aff2851cea
Nicolas Boulay <nicolas@boulay.name>
2022-06-19 10:47:26 +0200

    adding input args option input_file_name

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,25 @@ use std::process;
 mod csv;
 mod powerplant;
 
+use clap::Parser;
+
+/// electric mix simulator
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the input file
+    #[clap(short, long, value_parser)]
+    input_file_name: String,
+}
+
+
 fn main() {
+
+    let args = Args::parse();
+
+    println!("Input : {}", args.input_file_name);
+    
+
     if let Err(err) = csv::example() {
         println!("error running example: {}", err);
         process::exit(1);
